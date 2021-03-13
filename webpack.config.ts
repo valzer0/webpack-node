0a79113aeb2b6dbd0501051fc1b6197f361d939b
valzer0 <varzer0.mail@gmail.com>
2021-03-14 08:13:38 +0900

    udpate

---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+import path from 'path' 
+import webpack from 'webpack' 
+import nodeExternals from 'webpack-node-externals' 
+
+
+const config: webpack.Configuration = {
+  entry: {
+    main: path.resolve(__dirname, 'src/main')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `[name]-${process.env.NODE_ENV === 'production' ? 'min' : 'dev'}.js`
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  devtool: 'eval-source-map',
+  node: {
+    global: false,
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        exclude: /node_modules/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.ts','.js','.json']
+  }
+}
+
+export default config
